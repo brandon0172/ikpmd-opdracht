e64e1e712e8532aa525502efb696fac446a2a15e
Brandon van Wijk <brandonvanwijk@gmail.com>
2016-01-22 15:51:25 +0100

    added logo and button to mainactivity

--- app/src/main/java/nl/brandonvanwijk/ikpmd_eindopdracht/MainActivity.java
+++ app/src/main/java/nl/brandonvanwijk/ikpmd_eindopdracht/MainActivity.java
@@ -31,16 +31,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setSupportActionBar(toolbar);
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-
         if(isOnline() == false) {
             Toast.makeText(this, "Geen internetverbinding !", Toast.LENGTH_LONG).show();
         }
