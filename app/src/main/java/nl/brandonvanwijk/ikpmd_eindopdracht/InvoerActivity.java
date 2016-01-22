5e55453c6cecf9c7c41ba47e8b5cda68421d0ebc
Brandon van Wijk <brandonvanwijk@gmail.com>
2016-01-22 12:40:48 +0100

    changed textcolor of listview texviews to black

--- app/src/main/java/nl/brandonvanwijk/ikpmd_eindopdracht/InvoerActivity.java
+++ app/src/main/java/nl/brandonvanwijk/ikpmd_eindopdracht/InvoerActivity.java
@@ -8,6 +8,8 @@ import android.preference.PreferenceManager;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.Toolbar;
